b90a1bc26b172710b5092e9bd9c002e269a7ece5
Kuba Tuta <tuta.jakub@gmail.com>
2023-09-25 19:51:47 +0200

    initial state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [fv, setFv] = useState("");
+  const [plates, setPlates] = useState([""]);
+
+  const address = `https://dynamos.benelux.intra.corp/Archive/MainArchive.aspx?searchModus=2&contractnr=0&calcbasisvolgnr=0&nawnr=0&SleutelWaarde=${fv.trim()}&Caller=OnNotifyRowClick`;
+
+  const updatePlate = (index, value) => {
+    const updatedPlates = [...plates];
+    updatedPlates[index] = value;
+    setPlates(updatedPlates);
+  };
+
+  const handleInvoice = (event) => {
+    event.preventDefault();
+    window.open(address, "_blank");
+  };
+
+  const handleTotallLoss = (event, plate) => {
+    event.preventDefault();
+    const trimmedPlate = plate.trim();
+    const totallLossAddress = `https://serwisarval.pl/claims/insurancecase/vehicle-history-report?contract_plate_number=${trimmedPlate}&submitForm=Generuj+raport+PDF`;
+    window.open(totallLossAddress, "_blank");
+  };
+
+  const addPlate = (event) => {
+    event.preventDefault();
+    setPlates([...plates, ""]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      numer faktury
+      <form>
+        <input
+          type="text"
+          value={fv}
+          onChange={(event) => setFv(event.target.value)}
+        ></input>
+        <button onClick={(event) => handleInvoice(event)}>GO</button>
+      </form>
+      <br></br>
+      <br></br>
+      szkody
+      {plates.map((plate, index) => (
+        <div key={index}>
+          <form>
+            <input
+              type="text"
+              value={plate}
+              onChange={(event) => updatePlate(index, event.target.value)}
+            ></input>
+            <button onClick={(event) => handleTotallLoss(event, plate)}>
+              GO
+            </button>
+            <button onClick={(event) => addPlate(event)}>+</button>
+          </form>
+        </div>
+      ))}
     </div>
   );
 }
