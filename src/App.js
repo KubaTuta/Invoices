7f7bf6b3038ea3a307c7122609b2026edcccc2de
Kuba Tuta <tuta.jakub@gmail.com>
2023-09-26 19:42:00 +0200

    adding invoices +

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [fv, setFv] = useState("");
+  const [fv, setFv] = useState([""]);
   const [plates, setPlates] = useState([""]);
 
-  const address = `https://dynamos.benelux.intra.corp/Archive/MainArchive.aspx?searchModus=2&contractnr=0&calcbasisvolgnr=0&nawnr=0&SleutelWaarde=${fv.trim()}&Caller=OnNotifyRowClick`;
-
-  const updatePlate = (index, value) => {
-    const updatedPlates = [...plates];
-    updatedPlates[index] = value;
-    setPlates(updatedPlates);
-  };
-
-  const handleInvoice = (event) => {
+  const handleFv = (event, invoice) => {
     event.preventDefault();
+    const trimmedFv = invoice.trim();
+    const address = `https://dynamos.benelux.intra.corp/Archive/MainArchive.aspx?searchModus=2&contractnr=0&calcbasisvolgnr=0&nawnr=0&SleutelWaarde=${trimmedFv}&Caller=OnNotifyRowClick`;
     window.open(address, "_blank");
   };
 
@@ -25,22 +19,44 @@ function App() {
     window.open(totallLossAddress, "_blank");
   };
 
+const updateFv = (index, value) => {
+  const updatedFv = [...fv];
+  updatedFv[index] = value;
+  setFv(updatedFv)
+}
+
+  const updatePlate = (index, value) => {
+    const updatedPlates = [...plates];
+    updatedPlates[index] = value;
+    setPlates(updatedPlates);
+  };
+
   const addPlate = (event) => {
     event.preventDefault();
     setPlates([...plates, ""]);
   };
 
+  const addFv = (event) => {
+    event.preventDefault();
+    setFv([...fv, ""]);
+  };
+
   return (
     <div>
       numer faktury
-      <form>
-        <input
-          type="text"
-          value={fv}
-          onChange={(event) => setFv(event.target.value)}
-        ></input>
-        <button onClick={(event) => handleInvoice(event)}>GO</button>
-      </form>
+      {fv.map((invoice, index) => (
+        <div key={index}>
+          <form>
+            <input
+              type="text"
+              value={invoice}
+              onChange={(event) => updateFv(index, event.target.value)}
+            ></input>
+            <button onClick={(event) => handleFv(event, invoice)}>GO</button>
+            <button onClick={(event) => addFv(event)}>+</button>
+          </form>
+        </div>
+      ))}
       <br></br>
       <br></br>
       szkody
@@ -52,9 +68,7 @@ function App() {
               value={plate}
               onChange={(event) => updatePlate(index, event.target.value)}
             ></input>
-            <button onClick={(event) => handleTotallLoss(event, plate)}>
-              GO
-            </button>
+            <button onClick={(event) => handleTotallLoss(event, plate)}>GO</button>
             <button onClick={(event) => addPlate(event)}>+</button>
           </form>
         </div>
