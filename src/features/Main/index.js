171b8cbd7c69e63d87542710cb5fd37f0fa189e5
Kuba Tuta <tuta.jakub@gmail.com>
2023-10-09 16:30:31 +0200

    gathering up component

---
+++ src/features/Main/index.js
@@ -0,0 +1,20 @@
+import { Container } from "../../common/styles";
+import Plates from "../Plates";
+import NewInvoice from "../NewInvoice";
+
+import Input from "../Input";
+
+const Main = ({plates, setPlates}) => {
+  // const [plates, setPlates] = useState([""]);
+
+  return (
+    <Container>
+      {/* <img src={myImage} alt="Opis obrazu" /> */}    
+      <Input plates={plates} setPlates={setPlates} />
+      <Plates plates={plates} setPlates={setPlates} />
+      <NewInvoice plates={plates} setPlates={setPlates} />
+    </Container>
+  );
+};
+
+export default Main;
