71408dea80726cdf325b57ff268513e639e4c4fa
Kuba Tuta <tuta.jakub@gmail.com>
2023-10-10 20:23:29 +0200

    utilizing ready components

--- src/features/WordDistributor/index.js
+++ src/features/WordDistributor/index.js
@@ -1,14 +1,8 @@
 import { useState } from "react";
-import {
-  AStyled,
-  Container,
-  FormStyled,
-  InputStyled,
-} from "../../common/styles";
-import contracts from "../../contracts.json";
+import { ComboDiv, TextCombo } from "./styles";
+import Main from "../Main";
 
 const WordDistributor = () => {
-  const maintance = contracts;
   const [textarea, setTextarea] = useState("");
   const [plates, setPlates] = useState([]);
 
@@ -29,78 +23,14 @@ const WordDistributor = () => {
     setPlates(platesArray);
   };
 
-  const updateSinglePlate = (index, value) => {
-    const updatedPlates = [...plates];
-    updatedPlates[index] = value;
-    setPlates(updatedPlates);
-  };
-  const handleTotallLoss = (event, plate) => {
-    event.preventDefault();
-    const totallLossAddress = `https://serwisarval.pl/claims/insurancecase/vehicle-history-report?contract_plate_number=${plate}&submitForm=Generuj+raport+PDF`;
-    window.open(totallLossAddress, "_blank");
-  };
-
-  const handleHref = (plate) => {
-    const foundObject = maintance.find((item) => item[plate]);
-
-    let result = null;
-
-    if (foundObject) {
-      result = foundObject[plate];
-    } else {
-      result = "Nie znaleziono pasującego obiektu";
-    }
-
-    const maintanceAddress = `https://dynamos.benelux.intra.corp/Maintenance/ContractMaintenanceHistoryEntry.aspx?ContractNr=${result}&FromDashboard=1`;
-    return maintanceAddress;
-  };
-  const handleMaintance = (event, plate) => {
-    event.preventDefault();
-    const searchId = plate.trim().toUpperCase();
-    const foundObject = maintance.find((item) => item[searchId]);
-
-    let result = null;
-
-    if (foundObject) {
-      result = foundObject[searchId];
-    } else {
-      result = "Nie znaleziono pasującego obiektu";
-    }
-
-    const maintanceAddress = `https://dynamos.benelux.intra.corp/Maintenance/ContractMaintenanceHistoryEntry.aspx?ContractNr=${result}&FromDashboard=1`;
-    window.open(maintanceAddress, "_blank");
-  };
   return (
-    <Container>
-      <FormStyled>
-        <textarea value={textarea} onChange={updateTextarea}></textarea>
+    <ComboDiv>
+      <div>
+        <TextCombo value={textarea} onChange={updateTextarea}></TextCombo>
         <button onClick={distributWords}>GO</button>
-
-        {plates.map((plate, index) => (
-          <div key={index}>
-            <InputStyled
-              key={index}
-              type="text"
-              value={plate}
-              onChange={(event) => updateSinglePlate(index, event.target.value)}
-            ></InputStyled>
-            <AStyled
-              href={`https://serwisarval.pl/claims/insurancecase/vehicle-history-report?contract_plate_number=${plate}&submitForm=Generuj+raport+PDF`}
-              onClick={(event) => handleTotallLoss(event, plate)}
-            >
-              SZKODA
-            </AStyled>
-            <AStyled
-              href={handleHref(plate)}
-              onClick={(event) => handleMaintance(event, plate)}
-            >
-              SERWIS
-            </AStyled>
-            <br></br>
-          </div>
-        ))}
-      </FormStyled>
-    </Container>
+      </div>
+      <Main plates={plates} setPlates={setPlates} />
+    </ComboDiv>
   );
 };
 
