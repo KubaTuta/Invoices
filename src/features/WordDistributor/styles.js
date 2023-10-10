8c4f8c4864e795c1ed8c037e49eb8a770c25d526
Kuba Tuta <tuta.jakub@gmail.com>
2023-10-10 20:23:41 +0200

    general containers

---
+++ src/features/WordDistributor/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ComboDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+export const TextCombo = styled.textarea`
+  height-max: auto;
+`;
