555270bb1bb00a5ea1909e8ceb2807c4e949f8ca
Kuba Tuta <tuta.jakub@gmail.com>
2023-10-14 14:07:37 +0200

    basic styles

---
+++ src/features/KeyRack/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+  height: 30px;
+  width: 100px;
+  background: rgb(0, 120, 94);
+  margin: 1px;
+  padding-left: 5px;
+`;
+
+export const Layout = styled.div`
+display: flex;
+`
