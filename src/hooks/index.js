e20172b4a3966d7dbc0c73b82a63d4f6e15bcfa7
Kuba Tuta <tuta.jakub@gmail.com>
2023-11-19 12:59:08 +0100

    hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,89 @@
+var XLSX = require("xlsx");
+
+export const useHooks = (setData, file) => {
+  const handleConvertRecords = (event) => {
+    event.preventDefault();
+    if (file[0]) {
+      const fileReader = new FileReader();
+      fileReader.readAsBinaryString(file[0]);
+
+      fileReader.onload = (event) => {
+        const fileData = event.target.result;
+        const workbook = XLSX.read(fileData, { type: "binary" });
+        const resultArray = [];
+
+        workbook.SheetNames.forEach((sheetName) => {
+          const worksheet = workbook.Sheets[sheetName];
+          const range = XLSX.utils.decode_range(worksheet["!ref"]);
+          const rows = range.e.r;
+
+          for (let i = 1; i <= rows; i++) {
+            const cellA = worksheet[XLSX.utils.encode_cell({ r: i, c: 0 })];
+            const cellB = worksheet[XLSX.utils.encode_cell({ r: i, c: 36 })];
+            const cellC = worksheet[XLSX.utils.encode_cell({ r: i, c: 2 })];
+            const cellD = worksheet[XLSX.utils.encode_cell({ r: i, c: 37 })];
+
+            if (cellA) {
+              const plate = cellA.v;
+              const fvNumber = cellB ? cellB.v : "Brak";
+              const status = cellC ? cellC.v : "Brak";
+              const excelDate = cellD ? cellD.v : "Brak";
+              const invoiceIssue =
+                excelDate !== "Brak"
+                  ? new Date(
+                      (excelDate - 25569) * 86400000
+                    ).toLocaleDateString()
+                  : "Brak";
+              const obj = { id: i, plate, status, fvNumber, invoiceIssue };
+              resultArray.push(obj);
+            }
+          }
+        });
+        setData((prevData) => {
+          const dataArray = [...prevData];
+          dataArray[0] = resultArray;
+          return dataArray;
+        });
+      };
+    }
+  };
+
+  const handleConvertAuctionLossess = (event) => {
+    event.preventDefault();
+    if (file[1]) {
+      const fileReader = new FileReader();
+      fileReader.readAsBinaryString(file[1]);
+
+      fileReader.onload = (event) => {
+        const fileData = event.target.result;
+        const workbook = XLSX.read(fileData, { type: "binary" });
+        const resultArray = [];
+
+        workbook.SheetNames.forEach((sheetName) => {
+          const worksheet = workbook.Sheets[sheetName];
+          const range = XLSX.utils.decode_range(worksheet["!ref"]);
+          const rows = range.e.r;
+
+          for (let i = 1; i <= rows; i++) {
+            const cellA = worksheet[XLSX.utils.encode_cell({ r: i, c: 0 })];
+            const cellB = worksheet[XLSX.utils.encode_cell({ r: i, c: 1 })];
+
+            if (cellA) {
+              const plate = cellA.v;
+              const loss = cellB ? cellB.v : "nieaktualne";
+              const obj = { id: i, plate, loss };
+              resultArray.push(obj);
+            }
+          }
+        });
+        setData((prevData) => {
+          const dataArray = [...prevData];
+          dataArray[1] = resultArray;
+          return dataArray;
+        });
+      };
+    }
+  };
+
+  return {handleConvertRecords, handleConvertAuctionLossess, setData};
+};
